9331bd7f1f5b4c1a96188d8eda240e05bc1a0d1a
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-06-20 10:30:38 -0400

    oops

--- axefetch.go
+++ axefetch.go
@@ -129,7 +129,7 @@ func main() {
 				if conf.General.IP == "" {
 					return cli.Exit("no ip address given", 1)
 				}
-				statusReq, err := http.Get(fmt.Sprintf("http://%s/api/v2/system/status", conf.General.IP))
+				statusReq, err := http.Get(fmt.Sprintf("http://%s/api/system/info", conf.General.IP))
 				if err != nil {
 					return cli.Exit(fmt.Sprintf("error getting axe status: %s", err), 1)
 				}
@@ -142,7 +142,7 @@ func main() {
 				}
 
 				/// this gets unmarshalled into the same struct to fill the rest of the board info
-				asicReq, err := http.Get(fmt.Sprintf("http://%s/api/v2/system/board", conf.General.IP))
+				asicReq, err := http.Get(fmt.Sprintf("http://%s/api/system/asic", conf.General.IP))
 				if err != nil {
 					return cli.Exit(fmt.Sprintf("error getting axe info: %s", err), 1)
 				}
@@ -152,18 +152,6 @@ func main() {
 				}
 				if err := json.Unmarshal(body, &axeInfo); err != nil {
 					return cli.Exit(fmt.Sprintf("error unmarshalling axe info: %s", err), 1)
-				}
-
-				confReq, err := http.Get(fmt.Sprintf("http://%s/api/v2/system/config", conf.General.IP))
-				if err != nil {
-					return cli.Exit(fmt.Sprintf("error getting axe config: %s", err), 1)
-				}
-				body, err = io.ReadAll(confReq.Body)
-				if err != nil {
-					return cli.Exit(fmt.Sprintf("error reading axe config: %s", err), 1)
-				}
-				if err := json.Unmarshal(body, &axeInfo); err != nil {
-					return cli.Exit(fmt.Sprintf("error unmarshalling axe config: %s", err), 1)
 				}
 			} else {
 				axeInfo = testData
