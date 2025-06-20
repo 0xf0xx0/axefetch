0e0a631fa170c4c469fb5cdd4050814ed7f5531e
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-06-20 10:27:46 -0400

    better req errors

--- axefetch.go
+++ axefetch.go
@@ -131,14 +131,14 @@ func main() {
 				}
 				statusReq, err := http.Get(fmt.Sprintf("http://%s/api/system/info", conf.General.IP))
 				if err != nil {
-					return cli.Exit(fmt.Sprintf("error getting axe info: %s", err), 1)
+					return cli.Exit(fmt.Sprintf("error getting axe status: %s", err), 1)
 				}
 				body, err := io.ReadAll(statusReq.Body)
 				if err != nil {
-					return cli.Exit(fmt.Sprintf("error reading axe info: %s", err), 1)
+					return cli.Exit(fmt.Sprintf("error reading axe status: %s", err), 1)
 				}
 				if err := json.Unmarshal(body, &axeInfo); err != nil {
-					return cli.Exit(fmt.Sprintf("error unmarshalling axe info: %s", err), 1)
+					return cli.Exit(fmt.Sprintf("error unmarshalling axe status: %s", err), 1)
 				}
 				/// this gets unmarshalled into the same struct to fill the rest of the asic info
 				asicReq, err := http.Get(fmt.Sprintf("http://%s/api/system/asic", conf.General.IP))
