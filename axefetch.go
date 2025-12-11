4289e77c3be64f5ed253cf8c7c88a617ae9d9277
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-12-11 13:29:05 -0500

    split util funcs out to util.go

--- axefetch.go
+++ axefetch.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"axefetch/icons"
-	"axefetch/modules"
-	"axefetch/paths"
 	"axefetch/types"
 
 	"github.com/0xf0xx0/oigiki"
 	"github.com/fatih/color"
-	"github.com/pelletier/go-toml/v2"
 	"github.com/urfave/cli/v3"
 )
 
@@ -65,7 +58,7 @@ func main() {
 			&cli.StringFlag{
 				Name:  "conf",
 				Usage: "config file `path` (or 'none')",
-				Value: filepath.Join(paths.CONFIG_ROOT, "config.toml"),
+				Value: filepath.Join(getConfigDir(), "config.toml"),
 			},
 			&cli.StringFlag{
 				Name:  "ip",
@@ -88,7 +81,7 @@ func main() {
 				Hidden: true,
 			},
 			&cli.StringFlag{
-				Name:   "writedefaultconfig",
+				Name: "writedefaultconfig",
 			},
 		},
 		Action: func(_ context.Context, ctx *cli.Command) error {
@@ -206,7 +199,7 @@ func printIconAndInfo(icon, info []string, spacing int) {
 	infoLen := len(info)
 	if iconLen < infoLen {
 		/// strip color tags to get print length
-		repeat := strings.Repeat(" ", len(oigiki.StripTags(icon[iconLen-1])))
+		repeat := strings.Repeat(" ", len([]rune(oigiki.StripTags(icon[iconLen-1]))))
 		/// pad the icon slice
 		for diff := infoLen - iconLen; diff > 0; diff-- {
 			icon = append(icon, repeat)
@@ -225,6 +218,32 @@ func printIconAndInfo(icon, info []string, spacing int) {
 	}
 }
 
+// splits string, preserving quotes
+func splitFormatLine(line string) []string {
+	out := make([]string, 0, 4)
+	b := strings.Builder{}
+	quoteActive := false
+	escaped := false
+	for _, c := range line {
+		if c == '\\' {
+			escaped = true
+		} else if c == '"' && !escaped {
+			quoteActive = !quoteActive
+		} else if !quoteActive && c == ' ' {
+			out = append(out, b.String())
+			b.Reset()
+		} else {
+			b.WriteRune(c)
+			if escaped {
+				escaped = false
+			}
+		}
+	}
+	if b.Len() > 0 {
+		out = append(out, b.String())
+	}
+	return out
+}
 // processes the display format string and returns a slice of the (valid) lines
 func processFormat(format string, data types.ApiInfo) []string {
 	res := []string{}
@@ -251,8 +270,10 @@ func processFormat(format string, data types.ApiInfo) []string {
 			}
 		case "prin":
 			{
-				lastline = oigiki.TagString(strings.Join(args, " "), conf.ColorTheme.Info)
-				res = append(res, lastline)
+				if v := prin(args, oigiki.StripTags(lastline), data); v != "" {
+					lastline = v
+					res = append(res, v)
+				}
 				break
 			}
 		default:
@@ -264,6 +285,7 @@ func processFormat(format string, data types.ApiInfo) []string {
 	}
 	return res
 }
+
 func info(args []string, lastline string, data types.ApiInfo) string {
 	ret := ""
 	/// two formats: 'info <func>' and 'info <subtitle> <func>'
@@ -273,13 +295,13 @@ func info(args []string, lastline string, data types.ApiInfo) string {
 	case 1:
 		{
 			/// coloring for these is handled in each func
-			ret = modules.Modules[args[0]](conf, data, []string{lastline})
+			ret = Modules[args[0]](conf, data, []string{lastline})
 			break
 		}
 	/// <subtitle> <func>
 	case 2:
 		{
-			fn := modules.Modules[args[1]]
+			fn := Modules[args[1]]
 			if fn == nil {
 				return ret
 			}
@@ -299,97 +321,6 @@ func info(args []string, lastline string, data types.ApiInfo) string {
 	}
 	return ret
 }
-
-func loadConfig(path string, conf *types.Config) {
-	configfile, err := os.Open(path)
-	if err != nil {
-		println(fmt.Sprintf("failed to load config at %s: %s", path, err))
-		return
-	}
-	d := toml.NewDecoder(configfile)
-	d.DisallowUnknownFields()
-	if err := d.Decode(conf); err != nil {
-		println(fmt.Sprintf("failed to decode config at %s: %s", path, err))
-		os.Exit(1)
-	}
-}
-func writeDefaultConfig(path string) error {
-	conf, _ := toml.Marshal(types.DefaultConf)
-	if err := os.WriteFile(resolvePath(path), conf, 0755); err != nil {
-		return cli.Exit(fmt.Sprintf("couldnt create config file: %s", err), 1)
-	}
-	return nil
-}
-// resolves ~ and cleans path
-// https://stackoverflow.com/a/17617721
-func resolvePath(path string) string {
-	if strings.HasPrefix(path, "~") {
-		// Use strings.HasPrefix so we don't match paths like
-		// "/something/~/something/"
-		home, _ := os.UserHomeDir()
-		path = filepath.Join(home, path[1:])
-	}
-	return path
-}
-
-// just http.Get but errors on non 200 response and wraps up the root path
-func reqAPI(endpoint, ip string, unmarshalInto any) error {
-	req, err := http.Get(fmt.Sprintf("http://%s/api/%s", ip, endpoint))
-	if err != nil {
-		return err
-	}
-	if req.StatusCode != http.StatusOK {
-		return fmt.Errorf("%s", req.Status)
-	}
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		return err
-	}
-	if err := json.Unmarshal(body, unmarshalInto); err != nil {
-		return err
-	}
-	return nil
-}
-func copyConf(dest, src *types.Config) {
-	dest.Bestdiff = src.Bestdiff
-	dest.Chip = src.Chip
-	dest.ColorTheme = src.ColorTheme
-	dest.Display = src.Display
-	dest.Efficiency = src.Efficiency
-	dest.Firmware = src.Firmware
-	dest.General = src.General
-	dest.Hashrate = src.Hashrate
-	dest.Model = src.Model
-	dest.Pool = src.Pool
-	dest.Shares = src.Shares
-	dest.Temp = src.Temp
-	dest.Title = src.Title
-	dest.Uptime = src.Uptime
-}
-
-// / splits string, preserving quotes
-func splitFormatLine(line string) []string {
-	out := make([]string, 0, 4)
-	b := bytes.NewBuffer(make([]byte, 0, 16))
-	quoteActive := false
-	escaped := false
-	for _, c := range line {
-		if c == '\\' {
-			escaped = true
-		} else if c == '"' && !escaped {
-			quoteActive = !quoteActive
-		} else if !quoteActive && c == ' ' {
-			out = append(out, b.String())
-			b.Reset()
-		} else {
-			b.WriteRune(c)
-			if escaped {
-				escaped = false
-			}
-		}
-	}
-	if b.Len() > 0 {
-		out = append(out, b.String())
-	}
-	return out
+func prin(args []string, lastline string, data types.ApiInfo) string {
+	return oigiki.TagString(strings.Join(args, " "), conf.ColorTheme.Info)
 }
---
+++ util.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"axefetch/types"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/pelletier/go-toml/v2"
+	"github.com/urfave/cli/v3"
+)
+
+func loadConfig(path string, conf *types.Config) {
+	configfile, err := os.Open(path)
+	if err != nil {
+		println(fmt.Sprintf("failed to load config at %s: %s", path, err))
+		return
+	}
+	d := toml.NewDecoder(configfile)
+	d.DisallowUnknownFields()
+	if err := d.Decode(conf); err != nil {
+		println(fmt.Sprintf("failed to decode config at %s: %s", path, err))
+		os.Exit(1)
+	}
+}
+func writeDefaultConfig(path string) error {
+	conf, _ := toml.Marshal(types.DefaultConf)
+	if err := os.WriteFile(resolvePath(path), conf, 0755); err != nil {
+		return cli.Exit(fmt.Sprintf("couldnt create config file: %s", err), 1)
+	}
+	return nil
+}
+
+// resolves ~ and cleans path
+// https://stackoverflow.com/a/17617721
+func resolvePath(path string) string {
+	if strings.HasPrefix(path, "~") {
+		// Use strings.HasPrefix so we don't match paths like
+		// "/something/~/something/"
+		home, _ := os.UserHomeDir()
+		path = filepath.Join(home, path[1:])
+	}
+	return path
+}
+func getConfigDir() string {
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		println(fmt.Sprintf("error getting config dir: %s", err))
+		os.Exit(1)
+	}
+	return filepath.Join(userConfigDir, "./axefetch")
+}
+func copyConf(dest, src *types.Config) {
+	dest.Bestdiff = src.Bestdiff
+	dest.Chip = src.Chip
+	dest.ColorTheme = src.ColorTheme
+	dest.Display = src.Display
+	dest.Efficiency = src.Efficiency
+	dest.Firmware = src.Firmware
+	dest.General = src.General
+	dest.Hashrate = src.Hashrate
+	dest.Model = src.Model
+	dest.Pool = src.Pool
+	dest.Shares = src.Shares
+	dest.Temp = src.Temp
+	dest.Title = src.Title
+	dest.Uptime = src.Uptime
+}
+
+// just http.Get but errors on non 200 response and wraps up the root path
+func reqAPI(endpoint, ip string, unmarshalInto any) error {
+	req, err := http.Get(fmt.Sprintf("http://%s/api/%s", ip, endpoint))
+	if err != nil {
+		return err
+	}
+	if req.StatusCode != http.StatusOK {
+		return fmt.Errorf("%s", req.Status)
+	}
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, unmarshalInto); err != nil {
+		return err
+	}
+	return nil
+}
+
+// converts 123456 into 123.45K and 123456789876 into 123.45 G
+func floatToBinshort(value float64) string {
+	if value >= 1e12 {
+		return fmt.Sprintf("%.5gT", value/1e9) /// Trillions
+	} else if value >= 1e9 {
+		return fmt.Sprintf("%.5gG", value/1e9) /// Billions
+	} else if value >= 1e6 {
+		return fmt.Sprintf("%.5gM", value/1e6) /// Millions
+	} else if value >= 1e3 {
+		return fmt.Sprintf("%.5gK", value/1e3) /// Thousands
+	}
+	return fmt.Sprintf("%.5g", value) /// Less than a thousand
+}
+
+// units: gh/s, j/th, mhz, mv, c, ib, short
+func unitFormat(value float64, unit string) string {
+	switch unit {
+	case "gh/s":
+		{
+			unit = "GH"
+			if value > 1000 {
+				unit = "TH"
+				value /= 1000
+			}
+			return fmt.Sprintf("%.2f %s/s", value, unit)
+		}
+	case "j/th":
+		{
+			/// expected to be precalced
+			return fmt.Sprintf("%.3g J/TH", value)
+		}
+	case "mhz":
+		{
+			unit = "mHz"
+			if value > 1000 {
+				unit = "GHz"
+				value /= 1000
+			}
+			return fmt.Sprintf("%.5g %s", value, unit)
+		}
+	case "mv":
+		{
+			return fmt.Sprintf("%g mV", value)
+		}
+	case "c":
+		{
+			return fmt.Sprintf("%.2f C", value)
+		}
+	/// wtf is this format even called?
+	case "binshort":
+		{
+			return floatToBinshort(value)
+		}
+	case "short":
+		{
+			unit = ""
+			if value > 1e12 {
+				unit = "T"
+				value /= 1e12
+			} else if value > 1e9 {
+				unit = "B"
+				value /= 1e9
+			} else if value > 1e6 {
+				unit = "M"
+				value /= 1e6
+			} else if value > 1000 {
+				unit = "k"
+				value /= 1000
+			}
+			return fmt.Sprintf("%.3g%s", value, unit)
+		}
+	case "ib":
+		{
+			unit = "iB"
+			if value > 0x100000 {
+				unit = "MiB"
+				value /= 0x100000
+			} else if value > 1024 {
+				unit = "KiB"
+				value /= 1024
+			}
+			return fmt.Sprintf("%.3g %s", value, unit)
+		}
+	default:
+		{
+			return ""
+		}
+	}
+}
+
+// tries to get the worker name from the username string ('address.worker'),
+// otherwise truncates the address
+func getWorkerFromUser(username string) string {
+	split := strings.Split(username, ".")
+	if len(split) == 1 {
+		if len(split[0]) > 16 {
+			return fmt.Sprintf("%s...%s", username[:4], username[len(username)-4:])
+		}
+		return split[0]
+	}
+	return split[1]
+}
+func filterEmptyStringsOut(s []string) []string {
+	return slices.DeleteFunc(s, func(e string) bool {
+		if e == "" {
+			return true
+		}
+		return false
+	})
+}
+
+// prints a shortened data line
+// TODO: rename shouldBeShort
+func printWithShortpaw(str, shortpaw string, shouldBeShort bool) string {
+	if shouldBeShort {
+		return str
+	}
+	return fmt.Sprintf("%s %s", str, shortpaw)
+}
